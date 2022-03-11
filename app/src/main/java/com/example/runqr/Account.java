5704d9bdcac8278415cf5c269dcf770320b7e5f1
KennyUA <kke@ualberta.ca>
2022-03-10 21:32:55 -0700

    completed camera

--- app/src/main/java/com/example/runqr/Account.java
+++ app/src/main/java/com/example/runqr/Account.java
@@ -6,21 +6,22 @@ import java.util.ArrayList;
 public class Account  implements Serializable {
 
     private String username;
-    private PlayerStats playerStats;
     private String contactEmail;
 
-    private ArrayList<QRCode> QRLibrary;
+
 
     //public ArrayList<>
 
     public void Account(){
-        this.playerStats = new PlayerStats();
+        this.username = "test_username";
     }
 
 
-    public void addQRCode(QRCode newQRCode){
-        QRLibrary.add(newQRCode);
+    public String getUsername(){
+        return this.username;
     }
+
+
 
 
 
